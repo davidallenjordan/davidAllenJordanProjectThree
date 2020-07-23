eeb859559046eea31b0bbeab56ebf1af96c83219
David Allen-Jordan <davidallenjordan@gmail.com>
2020-07-23 17:37:13 -0400

    initial commit - HTML & SASS set up - started styling :rocket::rocket::smile_cat:

---
+++ script.js
@@ -0,0 +1,59 @@
+// Minimum Viable Product:
+// Create a What kind of _____ are you? Quiz
+
+
+// Display an intro screen with title, instructions and a button
+// Listen for the button click to move on to the first question
+// Display question and options in a form with radios and a submit button
+// Have four score categories stored inside an object or array
+// On form-submit update the correlating object key-value
+// Repeat for all questions
+// Determine which category has the highest score, either looping through object or sorting or if statements
+// Display the outcome with the highest score on the page
+
+
+
+// Loop through 5 possible outcomes to determine quiz answer
+    // If a category has the highest score 
+
+
+
+// consider using an array instead to store scores!!!
+
+
+const outcomes = {
+  Earth: 1,
+  Water: 1,
+  Wind: 2,
+  Fire: 1
+}
+// for (let element in outcomes) {
+//   console.log(outcomes[element]);
+// }
+
+// .val()
+
+
+// determine which category has the highest score
+
+// outcooomes.sort();
+// console.log(outcooomes);
+
+
+
+// const messy = outcomes.messy;
+
+
+// if (messy >= tidy & messy >= gourmet && messy >= basic)
+
+
+// for (let i = 0; i < 5; i++) {
+
+
+
+// }
+
+
+
+// Stretch Goals:
+// Timer counting down for each question, no point if it reaches 0
